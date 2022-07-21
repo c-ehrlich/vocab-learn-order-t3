25bd9d528369a34a3c5420b138ab580064551fc8
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-07-21 12:50:43 +0200

    Implement utilities

---
+++ src/server/resource/vocab/vocab.util.ts
@@ -0,0 +1,92 @@
+import { DuplicateWordList, FrequencyListWeights, Word } from './vocab.schema';
+
+// TODO use a map for this
+// TODO don't use ! types
+export function createCounts(
+  words: Word[],
+  duplicates: DuplicateWordList
+): Word[] {
+  const wordsWithMultiplier = words.map((word) => {
+    const index = duplicates.findIndex((w) => w.word === word.word);
+    if (index !== -1) {
+      word.multiplier = duplicates[index]!.count;
+      return word;
+    } else {
+      word.multiplier = 1;
+      return word;
+    }
+  });
+
+  console.log(wordsWithMultiplier);
+
+  return words;
+}
+
+// TODO use a map for this
+// TODO don't use ! types
+export default function findDuplicates(words: string[]): DuplicateWordList {
+  let duplicates: DuplicateWordList = [];
+
+  // create list of how often each word exists
+  words.forEach((word) => {
+    const index = duplicates.findIndex((dword) => dword.word === word);
+    if (index === -1) {
+      duplicates.push({ word, count: 1 });
+    } else {
+      duplicates[index]!.count++;
+    }
+  });
+
+  // delete all words that only exist once so the array is faster to search
+  for (let i = duplicates.length - 1; i >= 0; i--) {
+    if (duplicates[i]!.count === 1) {
+      duplicates = ([] as DuplicateWordList).concat(
+        duplicates.slice(0, i),
+        duplicates.slice(i + 1)
+      );
+    }
+  }
+
+  return duplicates;
+}
+
+export function sortWords(
+  words: Word[],
+  weights: FrequencyListWeights
+): Word[] {
+  return words.sort(compareWordForSorting);
+
+  // TODO refactor so compareWordForSorting doesn't have to live inside sortWords?
+  function compareWordForSorting(a: Word, b: Word): number {
+    return getWeightedWordRanking(a, weights) <
+      getWeightedWordRanking(b, weights)
+      ? 1
+      : -1;
+  }
+}
+
+function getWeightedWordRanking(
+  word: Word,
+  weights: FrequencyListWeights
+): number {
+  const baseValue =
+    frequencyListValueCalc(word.animeJDrama, weights.animeJDrama) +
+    frequencyListValueCalc(word.bccwj, weights.bccwj) +
+    frequencyListValueCalc(word.innocent, weights.innocent) +
+    frequencyListValueCalc(word.kokugojiten, weights.kokugojiten) +
+    frequencyListValueCalc(word.narou, weights.narou) +
+    frequencyListValueCalc(word.netflix, weights.netflix) +
+    frequencyListValueCalc(word.novels, weights.novels) +
+    frequencyListValueCalc(word.vn, weights.vn) +
+    frequencyListValueCalc(word.wikipedia, weights.wikipedia);
+
+  return baseValue * (word.multiplier || 1);
+}
+
+function frequencyListValueCalc(
+  flValue: number | undefined,
+  flWeight: number
+): number {
+  if (!flValue) return 0;
+  return Math.max(flWeight, 0) / flValue;
+}
