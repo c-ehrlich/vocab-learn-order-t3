9146e4699252fb6f7c3052a201cb244210f55c77
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-07-21 18:20:47 +0200

    feat: create index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,13 @@
+import { useAtom } from 'jotai';
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import { useState } from 'react';
 import SearchField from '../features/search/search-field';
-import { trpc } from '../utils/trpc';
+import SearchStateHandler from '../features/search/search-state-handler';
+import { isSearchingAtom } from '../utils/jotai';
 
 const Home: NextPage = () => {
-  const [isSearching, setIsSearching] = useState(false);
-  const words = trpc.proxy.vocab.learnOrder.useQuery({
-    words: ['食べる', '相談', '食事', '食券'],
-    weights: {
-      animeJDrama: 1,
-      bccwj: 1,
-      innocent: 1,
-      kokugojiten: 1,
-      narou: 1,
-      netflix: 1,
-      novels: 1,
-      vn: 1,
-      wikipedia: 1,
-    },
-  });
+  const [isSearching, __setIsSearching] = useAtom(isSearchingAtom);
 
-  return <>{isSearching ? <div>searching</div> : <SearchField />}</>;
+  return <>{isSearching ? <SearchStateHandler /> : <SearchField />}</>;
 };
 
 export default Home;
