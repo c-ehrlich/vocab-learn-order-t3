92cb31fe926b6b3928cc104d42a031a779b5754c
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-07-22 12:47:56 +0200

    feat: copy words to clipboard

--- src/features/app-shell/header.tsx
+++ src/features/app-shell/header.tsx
@@ -1,9 +1,11 @@
 // MUI imports
+import Alert from '@mui/material/Alert';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
+import Snackbar from '@mui/material/Snackbar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
@@ -13,18 +15,70 @@ import SettingsIcon from '@mui/icons-material/Settings';
 
 // Other imports
 import { useAtom } from 'jotai';
-import { isSearchingAtom } from '../../utils/jotai';
+import {
+  frequencyListWeightsAtom,
+  isSearchingAtom,
+  searchWordsAtom,
+} from '../../utils/jotai';
 import { useState } from 'react';
 import Sliders from './frequency-sliders';
 import MaterialModal from './material-modal';
 import HelpModalContents from './help-modal-contents';
 import { COLOR_LIGHT } from '../../theme/defaultTheme';
+import { trpc } from '../../utils/trpc';
 
 function Header() {
   const [isSearching, setIsSearching] = useAtom(isSearchingAtom);
+  const [frequencyListWeights, __setFrequencyListWeights] = useAtom(
+    frequencyListWeightsAtom
+  );
+  const [searchWords, __setSearchWords] = useAtom(searchWordsAtom);
+
   const [helpModalOpen, setHelpModalOpen] = useState(false);
+  const [successSnackbarOpen, setSuccessSnackbarOpen] = useState(false);
+  const [failureSnackbarOpen, setFailureSnackbarOpen] =
+    useState<boolean>(false);
   const [anchorElFrequency, setAnchorElFrequency] =
     useState<null | HTMLElement>(null);
+
+  const queryClient = trpc.useContext();
+
+  const temporarilyOpenSnackbar = (
+    openStateSetter: (value: React.SetStateAction<boolean>) => void
+  ) => {
+    openStateSetter(true);
+    setTimeout(() => {
+      openStateSetter(false);
+    }, 3000);
+  };
+
+  async function saveRemainingWordsToClipboard() {
+    const query = queryClient.getQueryData([
+      'vocab.learnOrder',
+      {
+        words: searchWords,
+        weights: frequencyListWeights,
+      },
+    ]);
+    if (query) {
+      let words = [] as string[];
+
+      query.words.forEach((word) => {
+        const multiplier = word.multiplier || 1;
+        for (let i = 0; i < multiplier; i++) {
+          words.push(word.word);
+        }
+      });
+      query.notFound.forEach((word) => words.push(word));
+      const text = words.join(', ');
+      await navigator.clipboard.writeText(text);
+      if ((await navigator.clipboard.readText()) === text) {
+        temporarilyOpenSnackbar(setSuccessSnackbarOpen);
+      } else {
+        temporarilyOpenSnackbar(setFailureSnackbarOpen);
+      }
+    }
+  }
 
   return (
     <AppBar position='sticky'>
@@ -85,10 +139,7 @@ function Header() {
             {isSearching ? (
               <IconButton
                 aria-label='save'
-                onClick={async () => {
-                  // TODO implement this (how with react query?)
-                  // saveRemainingWordsToClipboard();
-                }}
+                onClick={saveRemainingWordsToClipboard}
               >
                 <SaveAltIcon fontSize='large' />
               </IconButton>
@@ -109,6 +160,24 @@ function Header() {
           </MaterialModal>
         </Toolbar>
       </Container>
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={successSnackbarOpen}
+        onClose={() => setSuccessSnackbarOpen(false)}
+        sx={{ width: '100%' }}
+      >
+        <Alert severity='success'>Successfully copied to clipboard</Alert>
+      </Snackbar>
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={failureSnackbarOpen}
+        onClose={() => setFailureSnackbarOpen(false)}
+        sx={{ width: '100%' }}
+      >
+        <Alert severity='warning'>
+          Could not copy to clipboard. Please copy manually
+        </Alert>
+      </Snackbar>
     </AppBar>
   );
 }
